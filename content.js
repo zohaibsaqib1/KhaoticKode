21b2c6bb4b97e207581fd36c0ce6e13657b99b6e
zubyking1 <91407863+zubyking1@users.noreply.github.com>
2021-09-26 02:26:40 -0500

    Add files via upload

---
+++ content.js
@@ -0,0 +1,49 @@
+const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
+
+for (let i=0; i< text.length; i++) {
+    if (text[i].innerHTML.includes('Kanye')) {
+        text[i].innerHTML = text[i].innerHTML.replace('Kanye', 'The GOAT Kanye')
+
+    } else if (text[i].innerHTML.includes('West')) {
+        text[i].innerHTML = text[i].innerHTML.replace('West', 'The GOAT Kanye')
+    }
+
+    if (text[i].innerHTML.includes('the')) {
+        text[i].innerHTML = text[i].innerHTML.replace('the ', 'Seth Goldin')
+
+    }
+    else if (text[i].innerHTML.includes('The')) {
+        text[i].innerHTML = text[i].innerHTML.replace('The', 'Seth Goldin')
+    }
+    if (text[i].innerHTML.includes('hack')) {
+        text[i].innerHTML = text[i].innerHTML.replace('hack', 'Benjamin Thaw')
+
+    } 
+    if (text[i].innerHTML.includes('there')) {
+        text[i].innerHTML = text[i].innerHTML.replace('there', 'John Serrano')
+    } 
+    if (text[i].innerHTML.includes('will')) {
+        text[i].innerHTML = text[i].innerHTML.replace('will', 'Ciara O neil')
+
+    } 
+    if (text[i].innerHTML.includes('for')) {
+        text[i].innerHTML = text[i].innerHTML.replace('for ', 'Aden Eagle')
+
+    } 
+}
+
+
+
+
+alert('Im warning you, you will be stuck in a loop')
+alert('PRESS ALT F4 FOR FREE VBUCKS!!')
+alert('DIDNT I TELL YOU TO ALT F4!!')
+alert('Fine, Ill do it myself')
+new Audio('<url>').play('https://docs.google.com/file/d/0B-klwLEjaXWcZHR5SmJJWEwtYnc/edit?resourcekey=0-D33DYWMxjVde0g1m7qsoZw')
+window.location.replace('https://dscfau.com/static/media/sgoldin.dd395db7.png')
+
+
+
+
+
+
